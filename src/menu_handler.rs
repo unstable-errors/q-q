c9d8edd948de00000348062bf83f0e1ae91c1b01
electron271 <ahmedanzaldua@gmail.com>
2021-09-02 20:34:15 -0500

    edit

--- src/menu_handler.rs
+++ src/menu_handler.rs
@@ -1,17 +1,17 @@
 // Copyright (C) 2021 electron271
-// 
+//
 // This file is part of q>q.
-// 
+//
 // q>q is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // q>q is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with q>q.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -29,7 +29,6 @@ pub fn launch_app(app: &str) {
         other::python::python()
             .map_err(|err| println!("{:?}", err))
             .ok();
-        other::progress_bar_test::progress_bar_test();
     } else if app == "progress_bar_test" {
         other::progress_bar_test::progress_bar_test();
     } else if app == "clock" {
