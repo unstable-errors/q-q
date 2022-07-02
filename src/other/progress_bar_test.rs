d42ad33bd7fb54e26e38e220abf99634b521bd45
electron271 <ahmedanzaldua@gmail.com>
2022-07-02 15:36:55 -0500

    slower and more variable progress bar

--- src/other/progress_bar_test.rs
+++ src/other/progress_bar_test.rs
@@ -3,15 +3,23 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+// import libs:
+// std libs
 use std::cmp::min;
 use std::thread;
 use std::time::Duration;
 
+// random libs
+use rand::Rng;
+
+// progress bar libs
 use indicatif::{ProgressBar, ProgressStyle};
 
+// main function [progress_bar_test]
 pub fn progress_bar_test() {
     let mut downloaded = 0;
     let total_size = 231231231;
+    let mut rng = rand::thread_rng();
 
     let pb = ProgressBar::new(total_size);
     pb.set_style(ProgressStyle::default_bar()
@@ -22,7 +30,7 @@ pub fn progress_bar_test() {
         let new = min(downloaded + 223211, total_size);
         downloaded = new;
         pb.set_position(new);
-        thread::sleep(Duration::from_millis(12));
+        thread::sleep(Duration::from_millis(rng.gen_range(2..50)));
     }
 
     pb.finish_with_message("downloaded");
